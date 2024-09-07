f6004c220c1c5c5fc3f68941d2d10308bed4a0f2
axlehner <ax.lehner@gmail.com>
2024-09-07 16:37:45 -0500

    last commit? we cannot run this package because rasters cannot be stored in rda format without getting corrupted (only rds, and CRAN won't allow that format)

---
+++ R/agricultural_suitability.R
@@ -0,0 +1,33 @@
+#' Ramankutty et al. (2002)
+#'
+#' Agricultural suitability
+#'
+#'
+#' @name ramankuttyetal2002
+#' @format ##
+#' A raster, unprojected
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#'
+#' }
+#' @source Ramankutty, Navin, Jonathan A. Foley, John Norman, and Kevin McSweeney. "The Global Distribution of Cultivable Lands: Current Patterns and Sensitivity to Possible Climate Change." Global Ecology and Biogeography 11, no. 5 (2002): 377-392. https://doi.org/10.1046/j.1466-822x.2002.00294.x.
+NULL
+
+#' Lehner, Philippe (2024)
+#'
+#' Time-varying agricultural suitability, Europe
+#'
+#' @name agri_suit_1500_2000
+#' @format ##
+#' A rasterstack
+#' \describe{
+#'   \item{country}{Country name}
+#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
+#'   \item{year}{Year}
+#'   ...
+#' }
+#' @source Ramankutty, Navin, Jonathan A. Foley, John Norman, and Kevin McSweeney. "The Global Distribution of Cultivable Lands: Current Patterns and Sensitivity to Possible Climate Change." Global Ecology and Biogeography 11, no. 5 (2002): 377-392. https://doi.org/10.1046/j.1466-822x.2002.00294.x.
+NULL
