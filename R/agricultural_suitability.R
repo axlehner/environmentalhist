fb1502effc118b2440a33823f404aceacbb3bc84
axlehner <ax.lehner@gmail.com>
2024-09-10 18:40:57 -0500

    finished all documentation, vignette, readme

--- R/agricultural_suitability.R
+++ R/agricultural_suitability.R
@@ -1,33 +1,50 @@
-#' Ramankutty et al. (2002)
+#' Ramankutty et al. (2002) Index for Global Agricultural Suitability
 #'
-#' Agricultural suitability
+#' Agricultural suitability index scaled between 0 and 1.
+#' The dataset describes the fraction of each 0.5-degree grid cell occupied by cultivated land at a global level.
 #'
 #'
 #' @name ramankuttyetal2002
 #' @format ##
-#' A raster, unprojected
+#' A raster formatted as a stars object, unprojected EPSG:4326 (WGS84).
 #' \describe{
-#'   \item{country}{Country name}
-#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
-#'   \item{year}{Year}
-#'   ...
-#'
+#'   \item{cellvalue}{Agricultural suitability between 0 and 1.}
 #' }
 #' @source Ramankutty, Navin, Jonathan A. Foley, John Norman, and Kevin McSweeney. "The Global Distribution of Cultivable Lands: Current Patterns and Sensitivity to Possible Climate Change." Global Ecology and Biogeography 11, no. 5 (2002): 377-392. https://doi.org/10.1046/j.1466-822x.2002.00294.x.
+#'
+#' @examples
+#' if (requireNamespace("stars", quietly = TRUE)) {
+#'   library(stars)
+#'   data("ramankuttyetal2002")
+#'
+#'   plot(ramankutty)
+#'  }
 "ramankutty"
 
-#' Lehner, Philippe (2024)
+#' Lehner, Philippe (2024) Suitability Index for Europe from 1500 to 2000
 #'
-#' Time-varying agricultural suitability, Europe
+#' A gridded index at a 0.5-degree resolution at a yearly level.
+#' It relies on a simple surface energy and water balance model, focusing only on so-called exogenous geographic and climatic features.
+#' Consists of four main building blocks: a measure for cumulative temperature exposure (growing degree days), a measure for soil moisture (aridity index), the carbon content of the soil, and its potential hydrogen value. Land suitability is then defined as the predicted value of the propensity of a given piece of land to be suitable for cultivation.
 #'
-#' @name agri_suit_1500_2000
+#' @name agrisuit_1500_2000
 #' @format ##
-#' A rasterstack
+#' A GeoTIFF file formatted as a stars object, unprojected EPSG:4326 (WGS84). Each band represents one year.
 #' \describe{
-#'   \item{country}{Country name}
-#'   \item{iso2, iso3}{2 & 3 letter ISO country codes}
-#'   \item{year}{Year}
-#'   ...
+#'   \item{cellvalue}{Agricultural suitability between 0 and 1.}
+#'   \item{bands}{501 bands, each representing one year. Band 1 refers to year 1500, band 501 to year 2000.}
 #' }
-#' @source Ramankutty, Navin, Jonathan A. Foley, John Norman, and Kevin McSweeney. "The Global Distribution of Cultivable Lands: Current Patterns and Sensitivity to Possible Climate Change." Global Ecology and Biogeography 11, no. 5 (2002): 377-392. https://doi.org/10.1046/j.1466-822x.2002.00294.x.
+#' @source Lehner, Alexander, and Dylan Philippe. "A time-varying index for agricultural suitability across Europe from 1500 - 2000." (2024)
+#'
+#' @examples
+#' if (requireNamespace("stars", quietly = TRUE)) {
+#'   library(stars)
+#'   data("agrisuit_1500_2000")
+#'
+#'   # stack has 501 bands (one band for every year):
+#'   dim(suit1500200)
+#'   # plot year 1500:
+#'   plot(suit1500200[ , , ,1], main = "Agricultural Suitability in 1500")
+#'  }
+#'
 "suit1500200"
--- README.md
+++ README.md
@@ -6,7 +6,8 @@
 <!-- badges: start -->
 <!-- badges: end -->
 
-The goal of environmentalhist is to …
+The goal of `environmentalhist` is to make datasets from global
+environmental history easily available for teaching and research.
 
 ## Installation
 
@@ -18,41 +19,10 @@ You can install the development version of environmentalhist from
 devtools::install_github("axlehner/environmentalhist")
 ```
 
-## Example
+See the vignette (“getting started”) to learn how to access and use the
+included datasets. If you would
 
-This is a basic example which shows you how to solve a common problem:
+## Collaboration
 
-``` r
-library(environmentalhist)
-## basic example code
-```
-
-What is special about using `README.Rmd` instead of just `README.md`?
-You can include R chunks like so:
-
-``` r
-#summary(cars)
-```
-
-You’ll still need to render `README.Rmd` regularly, to keep `README.md`
-up-to-date. `devtools::build_readme()` is handy for this.
-
-You can also embed plots, for example:
-
-In that case, don’t forget to commit and push the resulting figure
-files, so they display on GitHub and CRAN.
-
-# COLLABORATION (@DYLAN)
-
-Open the urbanhist.Rproj Rproject in RStudio and make sure `devtools` is
-installed. Then you just hit ‘Cmd + Shift + B’ every time you put a new
-function in the R folder or change something. That installs the package
-onto your disk and you have it globally everywhere.
-
-For the data storage and documentation I need to readup on best
-practices in R packages. There is always a `\data` and a `\data-raw`
-folder
-
-Descriptions are in the `\vignettes` folder
-
-That’s it. Don’t worry about the rest for now.
+If you would like to add or suggest a dataset, please submit a pull
+request after forking the project.
